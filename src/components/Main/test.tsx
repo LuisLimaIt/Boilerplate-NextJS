f6d82c7a42c9c343cc97a3a425c8ae6d58c87767
LuisLimaIt <pls.luis@gmail.com>
2021-10-26 10:01:36 -0300

    Automatizando criação de arquivos

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from '@testing-library/react'
+
 import Main from '.'
+
 describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
